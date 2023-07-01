122b34c5c8989bd7589b784e040425337b7f9bac
Marcus Kammer <marcus.kammer@mailbox.org>
2023-07-01 11:28:17 +0200

    Update buttons

--- src/buttons.lisp
+++ src/buttons.lisp
@@ -1,6 +1,6 @@
 (in-package :cl-sbt)
 
-(defmacro sbt-btn ((&key (type "default") (size nil)) &body body)
+(defmacro sbt-btn ((&key (type nil) (size nil)) &body body)
   `(spinneret:with-html
      (:button :type "button"
               :class (concatenate 'string "btn"
