4ac4274cea201ab3be2957e5b5392908b97c6e90
Marcus Kammer <marcus.kammer@mailbox.org>
2023-07-02 09:15:20 +0200

    Update dropdowns

--- src/dropdowns.lisp
+++ src/dropdowns.lisp
@@ -1,1 +1,20 @@
 (in-package :cl-sbt)
+
+(defmacro dropdown ((&key (title "")) &body body)
+  `(spinneret:with-html
+     (:div :class "dropdown"
+           (:button :class "btn btn-secondary dropdown-toggle"
+                    :type "button"
+                    :data-bs-toggle "dropdown"
+                    :aria-expanded "false"
+                    ,title)
+           ,@body)))
+
+(defmacro dropdown-menu (&body body)
+  `(spinneret:with-html
+     (:ul :class "dropdown-menu"
+          ,@body)))
+
+(defmacro dropdown-item (&body body)
+  `(spinneret:with-html
+     (:li (:a :class "dropdown-item" :href "#" ,@body))))
