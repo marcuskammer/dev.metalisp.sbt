b232c121c314f15ee203feeed47afdaabae034b5
Marcus Kammer <marcus.kammer@mailbox.org>
2023-07-08 14:31:53 +0200

    Fix indentation for accordion

--- src/component/accordion.lisp
+++ src/component/accordion.lisp
@@ -1,23 +1,25 @@
 (in-package :cl-sbt)
 
-(defmacro accordion-header (&body body)
+(defmacro accordion-header (target title)
   `(spinneret:with-html
-       (:h2 :class "accordion-header"
-                  (:button :class "accordion-button"
-                           :type "button"
-                           :data-bs-toggle="collapse"
-                           :data-bs-target="#collapseOne"
-                           :aria-expanded="true"
-                           :aria-controls="collapseOne"
-                           ,@body))))
+     (:h2 :class "accordion-header"
+          (:button :class "accordion-button"
+                   :type "button"
+                   :data-bs-toggle "collapse"
+                   :data-bs-target (format nil "#~a" ,target)
+                   :aria-expanded "true"
+                   :aria-controls "collapseOne"
+                   ,title))))
 
 (defmacro accordion-item (&body body)
-    `(spinneret:with-html
-       (:div :class "accordion-item"
-             ,@body)))
+  `(spinneret:with-html
+     (:div :class "accordion-item"
+           ,@body)))
 
-(defmacro accordion ((&key (id nil)) &body body)
-    `(spinneret:with-html
-       (:div :class "accordion"
-             :id ,id
-             ,@body)))
+(defmacro accordion ((&key (id "accordionExample")) &rest rest)
+  `(spinneret:with-html
+     (:div :class "accordion"
+           :id ,id
+           ,@(loop for item in rest
+                   collect (destructuring-bind (&key id title) item
+                             `(accordion-item (accordion-header ,id ,title)))))))
