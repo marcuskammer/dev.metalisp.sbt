57a33029e3df435be2e080df05e3af0a57705de9
Marcus Kammer <marcus.kammer@mailbox.org>
2023-07-08 22:09:06 +0200

    Update accordion

--- src/component/accordion.lisp
+++ src/component/accordion.lisp
@@ -2,16 +2,24 @@
 
 (in-package :cl-sbt)
 
-(defmacro accordion-header (target name)
+(defmacro accordion-header (target name show)
   `(spinneret:with-html
      (:h2 :class "accordion-header"
           (:button :class "accordion-button"
                    :type "button"
                    :data-bs-toggle "collapse"
                    :data-bs-target (format nil "#~a" ,target)
-                   :aria-expanded "true"
-                   :aria-controls "collapseOne"
+                   :aria-expanded ,(if show "true" "false")
+                   :aria-controls (format nil "#~a" ,target)
                    ,name))))
+
+(defmacro accordion-collapse (parent id show &body body)
+  `(spinneret:with-html
+     (:div :id ,id
+           :class ,(concatenate 'string "accordion-collapse collapse" (when show " show"))
+           :data-bs-parent (format nil "#~a" ,parent)
+           (:div :class "accordion-body"
+                 ,@body))))
 
 (defmacro accordion-item (&body body)
   `(spinneret:with-html
@@ -23,5 +31,13 @@
      (:div :class "accordion"
            :id ,id
            ,@(loop for item in rest
-                   collect (destructuring-bind (&key target name) item
-                             `(accordion-item (accordion-header ,target ,name)))))))
+                   collect (destructuring-bind (&key target name show content) item
+                             `(accordion-item (accordion-header ,target ,name ,show)
+                                (accordion-collapse ,id ,target ,show ,content)))))))
+
+(defun show-accordion-example ()
+  "Show an generated accordion example"
+  (accordion (:id "accordionExample")
+             (:target "collapseOne" :name "Accordion Item #1" :show t :content "This is the first item's accordion body.")
+             (:target "collapseTwo" :name "Accordion Item #2" :content "This is the second item's accordion body.")
+             (:target "collapseThree" :name "Accordion Item #3" :content "This is the second item's accordion body.")))
