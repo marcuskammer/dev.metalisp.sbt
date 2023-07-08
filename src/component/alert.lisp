82c0e3eb1f1b20e0f9b07ca31d00ab21011d40d0
Marcus Kammer <marcus.kammer@mailbox.org>
2023-07-08 22:25:20 +0200

    Update alerts

--- src/component/alert.lisp
+++ src/component/alert.lisp
@@ -2,13 +2,17 @@
 
 (defmacro alert ((&key (type nil) (dismissible nil)) &body body)
   `(spinneret:with-html
-       (:div :class ,(if dismissible
-                         (format nil "alert alert-~a alert-dismissible" type)
-                         (format nil "alert alert-~a" type)) :role "alert"
-                         ,(when dismissible
-                            `(:button :type "button" :class "close" :data-dismiss "alert" :aria-label "Close"
-                                      (:span :aria-hidden "true" "&times;")))
-                         ,@body)))
+     (:div :role "alert"
+           :class ,(if dismissible
+                       (format nil "alert alert-~a alert-dismissible" type)
+                       (format nil "alert alert-~a" type))
+           ,(when dismissible
+              `(:button :type "button"
+                        :class "close"
+                        :data-dismiss "alert"
+                        :aria-label "Close"
+                        (:span :aria-hidden "true" "&times;")))
+           ,@body)))
 
 (defmacro alert-primary (&body body)
   `(alert (:type "primary") ,@body))
