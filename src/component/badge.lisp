4c21a5049cc3f098c8880de7015c10e0ad2c74aa
Marcus Kammer <marcus.kammer@mailbox.org>
2023-07-09 09:33:01 +0200

    Update badges

--- src/component/badge.lisp
+++ src/component/badge.lisp
@@ -1,12 +1,37 @@
+;; https://getbootstrap.com/docs/5.3/components/badge/
+
 (in-package :cl-sbt)
 
 (defmacro badge ((&key (classes nil)) &body body)
   "https://getbootstrap.com/docs/5.3/components/badge/
 Documentation and examples for badges, our small count and labeling component."
   `(spinneret:with-html
-     (:span :class (concatenate 'string "badge"
-                                (if ,classes (format nil " ~a" ,classes)))
+     (:span :class (concatenate 'string "badge " ,classes)
             ,@body)))
+
+(defmacro badge-primary (&body body)
+  `(badge (:classes "text-bg-primary") ,@body))
+
+(defmacro badge-secondary (&body body)
+  `(badge (:classes "text-bg-secondary") ,@body))
+
+(defmacro badge-success (&body body)
+  `(badge (:classes "text-bg-success") ,@body))
+
+(defmacro badge-danger (&body body)
+  `(badge (:classes "text-bg-danger") ,@body))
+
+(defmacro badge-warning (&body body)
+  `(badge (:classes "text-bg-warning") ,@body))
+
+(defmacro badge-info (&body body)
+  `(badge (:classes "text-bg-info") ,@body))
+
+(defmacro badge-light (&body body)
+  `(badge (:classes "text-bg-light") ,@body))
+
+(defmacro badge-dark (&body body)
+  `(badge (:classes "text-bg-dark") ,@body))
 
 (defmacro badge-pill-primary (&body body)
   `(badge (:classes "rounded-pill text-bg-primary") ,@body))
