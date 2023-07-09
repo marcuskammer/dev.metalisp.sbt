b7501996cad1ab8e44ea4c3c01cec5d7f05f1872
Marcus Kammer <marcus.kammer@mailbox.org>
2023-07-09 09:03:27 +0200

    Update alerts

--- src/component/alert.lisp
+++ src/component/alert.lisp
@@ -1,17 +1,26 @@
+;; https://getbootstrap.com/docs/5.3/components/alerts/
+
+;; Provide contextual feedback messages for typical user actions with the handful
+;; of available and flexible alert messages. Alerts are available for any length
+;; of text, as well as an optional close button. For proper styling, use one of
+;; the eight required contextual classes (e.g., .alert-success).
+
 (in-package :cl-sbt)
+
+(defmacro alert-btn ()
+  `(spinneret:with-html
+     (:button :type "button"
+              :class "btn-close"
+              :data-bs-dismiss "alert"
+              :aria-label "Close")))
 
 (defmacro alert ((&key (type nil) (dismissible nil)) &body body)
   `(spinneret:with-html
      (:div :role "alert"
-           :class ,(if dismissible
-                       (format nil "alert alert-~a alert-dismissible" type)
-                       (format nil "alert alert-~a" type))
+           :class ,(concatenate 'string (format nil "alert alert-~a" type)
+                                (when dismissible " alert-dismissible"))
            ,(when dismissible
-              `(:button :type "button"
-                        :class "close"
-                        :data-dismiss "alert"
-                        :aria-label "Close"
-                        (:span :aria-hidden "true" "&times;")))
+              `(alert-btn))
            ,@body)))
 
 (defmacro alert-primary (&body body)
@@ -37,3 +46,27 @@
 
 (defmacro alert-dark (&body body)
   `(alert (:type "dark") ,@body))
+
+(defmacro alert-primary-dismiss (&body body)
+  `(alert (:type "primary" :dismissible t) ,@body))
+
+(defmacro alert-secondary-dismiss (&body body)
+  `(alert (:type "secondary" :dismissible t) ,@body))
+
+(defmacro alert-success-dismiss (&body body)
+  `(alert (:type "success" :dismissible t) ,@body))
+
+(defmacro alert-danger-dismiss (&body body)
+  `(alert (:type "danger" :dismissible t) ,@body))
+
+(defmacro alert-warning-dismiss (&body body)
+  `(alert (:type "warning" :dismissible t) ,@body))
+
+(defmacro alert-info-dismiss (&body body)
+  `(alert (:type "info" :dismissible t) ,@body))
+
+(defmacro alert-light-dismiss (&body body)
+  `(alert (:type "light" :dismissible t) ,@body))
+
+(defmacro alert-dark-dismiss (&body body)
+  `(alert (:type "dark" :dismissible t) ,@body))
