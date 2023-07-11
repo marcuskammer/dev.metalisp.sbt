6ac8ed1073b3321b62971e9201b45a75e1afea1d
Marcus Kammer <marcus.kammer@mailbox.org>
2023-07-11 18:48:32 +0200

    Define package card

--- src/component/card.lisp
+++ src/component/card.lisp
@@ -6,49 +6,60 @@
 ;; replace our old panels, wells, and thumbnails. Similar functionality to those
 ;; components is available as modifier classes for cards.
 
-(in-package :cl-sbt)
+(defpackage cl-sbt-card
+  (:use :cl)
+  (:export
+   :title
+   :subtitle
+   :text
+   :link
+   :header
+   :img
+   :body
+   :card-with-img
+   :card))
 
-(defmacro card-title (&body body)
-  "Card titles are used by adding .card-title to a <h*> tag."
-  `(spinneret:with-html
-     (:h5 :class "card-title" ,@body)))
+(in-package :cl-sbt-card)
 
-(defmacro card-subtitle (&body body)
+(defmacro title (&body body)
   `(spinneret:with-html
-     (:h6 :class "card-subtitle mb-2 text-body-secondary" ,@body)))
+       (:h5 :class "card-title" ,@body)))
 
-(defmacro card-text (&body body)
+(defmacro subtitle (&body body)
   `(spinneret:with-html
-     (:p :class "card-text" ,@body)))
+       (:h6 :class "card-subtitle mb-2 text-body-secondary" ,@body)))
 
-(defmacro card-link (&body body)
+(defmacro text (&body body)
   `(spinneret:with-html
-     (:a :href "#" :class "card-link" ,@body)))
+       (:p :class "card-text" ,@body)))
 
-(defmacro card-header (&body body)
+(defmacro link ((&key (href "#")) &body body)
   `(spinneret:with-html
-     (:div :class "header" ,@body)))
+       (:a :href ,href :class "card-link" ,@body)))
 
-(defmacro card-img ((&key (src nil) (alt nil)))
-  ".card-img-top places an image to the top of the card."
+(defmacro header (&body body)
   `(spinneret:with-html
-     (:img :src ,src
-           :alt ,alt
-           :class "card-img-top")))
+       (:div :class "header" ,@body)))
 
-(defmacro card-body (&body body)
+(defmacro img ((&key (src "#") (alt "Card Image")))
   `(spinneret:with-html
-     (:div :class "card-body" ,@body)))
+       (:img :src ,src
+             :alt ,alt
+             :class "card-img-top")))
 
-(defmacro card ((&key (img-src nil)) &body body)
+(defmacro body (&body body)
   `(spinneret:with-html
-     (:div :class "card"
-           ,(if (null img-src) nil `(:img :class "card-img-top" :src ,img-src))
-           (:div :class "card-body" ,@body))))
+       (:div :class "card-body" ,@body)))
 
-(defmacro card-* (&body body)
+(defmacro card-with-img ((&key (img-src nil)) &body body)
   `(spinneret:with-html
-     (:div :class "card" ,@body)))
+       (:div :class "card"
+             ,(if (null img-src) nil `(img (:src ,img-src)))
+             (:div :class "card-body" ,@body))))
+
+(defmacro card (&body body)
+  `(spinneret:with-html
+       (:div :class "card" ,@body)))
 
 ;; Kitchen sink
 ;; Mix and match multiple content types to create the card you need, or throw
@@ -56,11 +67,11 @@
 ;; list group—all wrapped in a fixed-width card.
 
 (defun card-kitchen-sink ()
-  (card-* (card-img (:src "..."))
-    (card-body (card-title "Card title")
-               (card-text "Some quick example text to build on the card title and make up the bulk of the card's content."))
-    (list-group (:content "An item")
-                (:content "A second item")
-                (:content "A third item"))
-    (card-body (card-link "Card Link")
-               (card-link "Another Link"))))
+  (card (img (:src "..."))
+        (body (card-title "Card title")
+              (card-text "Some quick example text to build on the card title and make up the bulk of the card's content."))
+        (cl-sbt-list-group (:content "An item")
+                           (:content "A second item")
+                           (:content "A third item"))
+        (body (card-link "Card Link")
+              (card-link "Another Link"))))
