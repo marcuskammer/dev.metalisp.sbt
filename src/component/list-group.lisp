5d167d07205055f783be0c2023676e9a8b39337f
Marcus Kammer <marcus.kammer@mailbox.org>
2023-07-14 16:07:27 +0200

    Update docs

--- src/component/list-group.lisp
+++ src/component/list-group.lisp
@@ -1,25 +1,52 @@
-;; List groups
-;; Create lists of content in a card with a flush list group.
+;;https://getbootstrap.com/docs/5.3/components/list-group/
+
+;; Bootstrap's List Group component is a flexible and versatile component used
+;; for displaying a series of content which can be a mix of various types, like
+;; text, links, buttons, etc., in a list format. This component is typically
+;; used for showing a list of items within a card, a sidebar, or a dropdown
+;; menu.
+
+;; The component is highly customizable, allowing for different styles and
+;; effects. By default, the List Group items are styled as plain text, but can
+;; be modified into clickable elements or buttons. The List Group component
+;; also supports contextual classes for styling list items with different
+;; states, such as success, warning, danger, and so on.
+
+;; In addition to this, the List Group component supports adding badges to list
+;; items, making it suitable for a wide range of applications, such as a list
+;; of emails with the number of unread messages, a list of tasks with their
+;; completion status, and so on.
 
 (defpackage cl-sbt-list-group
   (:use :cl)
   (:export
    :item
-   :list-group))
+   :list-group)
+(:documentation "A Common Lisp package for generating Bootstrap List Group components. This package provides macros for generating Bootstrap List Group components, a versatile element used to display a series of content in a list format. The `item` macro creates a Bootstrap list group item, which can contain any series of forms. The `list-group` macro generates a Bootstrap list group, which is a collection of list group items. Bootstrap's List Group components are highly customizable and can be used in a wide range of scenarios. They can be displayed as plain text or clickable elements, support adding badges, and can be styled with different contextual classes."))
 
 (in-package :cl-sbt-list-group)
 
 (defmacro item (&body body)
   "This macro generates a Bootstrap list group item.
 
-BODY: The contents of the list group item."
+BODY: The contents of the list group item. It should be a sequence of forms
+which will be used as the contents of the list group item.
+
+Example usage:
+(item \"This is a list group item\")
+(item (:p \"This is a list group item\"))"
   `(spinneret:with-html
      (:li :class "list-group-item" ,@body)))
 
 (defmacro list-group (&rest rest)
   "This macro generates a Bootstrap list group.
 
-REST: A sequence of items to be included in the list group. Each item is a keyword-value pair, where the keyword is ':content' and the value is the content of the item."
+REST: A sequence of items to be included in the list group. Each item is a
+keyword-value pair, where the keyword is ':content' and the value is the
+content of the item.
+
+Example usage:
+(list-group (:content \"First item\") (:content \"Second item\"))"
   `(spinneret:with-html
      (:ul :class "list-group list-group-flush"
           ,@(loop for item in rest
