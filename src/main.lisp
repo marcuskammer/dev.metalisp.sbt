f0b0ffa3d150eda4bed4531e8bde181c5302d90f
Marcus Kammer <marcus.kammer@mailbox.org>
2023-07-16 13:57:42 +0200

    Remove page lisp

--- src/main.lisp
+++ src/main.lisp
@@ -1,9 +1,22 @@
 (defpackage cl-sbt
   (:use :cl)
   (:export
-   :write-string-to-file))
+   :write-string-to-file
+   :with-page))
 
 (in-package :cl-sbt)
+
+(defmacro with-page ((&key title) &body body)
+  `(spinneret:with-html
+     (:doctype)
+     (:html
+      (:head
+       (:title ,title)
+       (:link
+           :type "text/css"
+         :rel "stylesheet"
+         :href "public/5.3.0/bootstrap.min.css"))
+      (:body ,@body))))
 
 (defun write-string-to-file (filename string)
   (with-open-file (stream filename :direction :output :if-exists :supersede)
