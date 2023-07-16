101ae7f40a7c5dc206e6ef2c2e4964e6ca324907
Marcus Kammer <marcus.kammer@mailbox.org>
2023-07-16 22:20:32 +0200

    Update spinner package; add examples to docs

--- src/component/spinner.lisp
+++ src/component/spinner.lisp
@@ -25,27 +25,60 @@
    :spinner-border-danger
    :spinner-border-warning
    :spinner-border-info
-   :spinner-border-warning
    :spinner-border-light
    :spinner-border-dark
+   :spinner-border-primary-sm
+   :spinner-border-secondary-sm
+   :spinner-border-success-sm
+   :spinner-border-danger-sm
+   :spinner-border-info-sm
+   :spinner-border-warning-sm
+   :spinner-border-light-sm
+   :spinner-border-dark-sm
    :spinner-grow-primary
    :spinner-grow-secondary
    :spinner-grow-success
    :spinner-grow-danger
-   :spinner-grow-warning
    :spinner-grow-info
    :spinner-grow-warning
    :spinner-grow-light
-   :spinner-grow-dark)
-  (:documentation "This package provides a set of macros to generate Bootstrap Spinner components. Bootstrap Spinners are versatile components used to indicate a loading state in a variety of scenarios. They can be displayed when the user initiates an action that requires some time to process, such as submitting a form or loading data. Spinners are available in two styles: Border and Grow. The Border style is a rotating border, while the Grow style is a shape that continuously grows and shrinks. Through these macros, the color of the spinner can be specified as 'primary', 'secondary', 'success', 'danger', 'warning', 'info', 'light', 'dark'."))
+   :spinner-grow-dark
+   :spinner-grow-primary-sm
+   :spinner-grow-secondary-sm
+   :spinner-grow-success-sm
+   :spinner-grow-danger-sm
+   :spinner-grow-info-sm
+   :spinner-grow-warning-sm
+   :spinner-grow-light-sm
+   :spinner-grow-dark-sm)
+  (:documentation "This package provides a set of macros to generate Bootstrap Spinner components."))
 
 (in-package :cl-sbt-spinner)
 
 (defmacro spinner ((&key (type "border") (color "primary") (size nil)))
-  "This macro generates a Bootstrap spinner with a specified color.
+  "This macro generates a Bootstrap spinner with a specified type, color, and size.
 
 TYPE: Specifies the spinner style. Can be 'border' or 'grow'. Defaults to 'border'.
-COLOR: Specifies the spinner color. Can be 'primary', 'secondary', 'success', 'danger', 'warning', 'info', 'light', 'dark', or 'link'. Defaults to 'primary'."
+
+COLOR: Specifies the spinner color. Can be 'primary', 'secondary', 'success',
+'danger', 'warning', 'info', 'light', 'dark', or 'link'. Defaults to
+'primary'.
+
+SIZE: Specifies the size of the spinner. Can be 'sm' for small or NIL for
+default size.
+
+Examples:
+  (spinner (:type \"border\" :color \"danger\" :size \"sm\"))
+  ; Generates a small, red, border spinner
+
+  (spinner (:type \"grow\" :color \"success\"))
+  ; Generates a green, growing spinner with default size
+
+  (spinner (:type \"border\" :color \"primary\"))
+  ; Generates a blue, border spinner with default size
+
+  (spinner (:type \"grow\" :color \"warning\" :size \"sm\"))
+  ; Generates a small, yellow, growing spinner"
   `(spinneret:with-html
      (:div :class ,(if (null size)
                        (format nil "spinner-~a text-~a" type color)
@@ -54,10 +87,27 @@ COLOR: Specifies the spinner color. Can be 'primary', 'secondary', 'success', 'd
            (:span :class "visually-hidden" "Loading..."))))
 
 (defmacro define-spinner (type color size)
-  "This macro defines a new spinner macro with a specified style and color.
+  "This macro defines a new spinner macro with a specified type, color, and size.
 
 TYPE: Specifies the style of the spinner. It can be 'border' or 'grow'.
-COLOR: Specifies the color of the spinner."
+
+COLOR: Specifies the color of the spinner.
+
+SIZE: Specifies the size of the spinner. It can be 'sm' for small or NIL for
+default size.
+
+Examples:
+  (define-spinner \"border\" \"danger\" \"sm\")
+  ; Defines a macro for a small, red, border spinner
+
+  (define-spinner \"grow\" \"success\" NIL)
+  ; Defines a macro for a green, growing spinner with default size
+
+  (define-spinner \"border\" \"primary\" NIL)
+  ; Defines a macro for a blue, border spinner with default size
+
+  (define-spinner \"grow\" \"warning\" \"sm\")
+  ; Defines a macro for a small, yellow, growing spinner"
   (let* ((size-name (if (null size) "" (format nil "-~a" size)))
          (macro-name (intern (string-upcase (concatenate 'string "SPINNER-" type "-" color size-name)))))
     `(defmacro ,macro-name ()
@@ -66,7 +116,11 @@ COLOR: Specifies the color of the spinner."
 (defmacro define-spinners (names)
   "This macro defines a set of new spinner macros.
 
-NAMES: A list of colors to use for the spinners."
+NAMES: A list of colors to use for the spinners.
+
+Example:
+  (define-spinners (\"primary\" \"secondary\" \"success\" \"danger\" \"warning\" \"info\" \"light\" \"dark\"))
+  ; Defines 32 new macros (8 colors * 2 types * 2 sizes)"
   `(progn
      ,@(loop for item in names
              for color = (string-downcase (string item))
