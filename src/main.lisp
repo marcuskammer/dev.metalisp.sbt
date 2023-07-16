c6e42f7d9aaf5bc966e86b54e2664f7d20cf1c7a
Marcus Kammer <marcus.kammer@mailbox.org>
2023-07-16 11:36:30 +0200

    Clean up main file

--- src/main.lisp
+++ src/main.lisp
@@ -1,36 +1,10 @@
 (defpackage cl-sbt
   (:use :cl)
   (:export
-   :container
-   :col-md
-   :col-xs
-   :col-sm
-   :col-lg
-   :icon
-   :well
-   :with-page))
+   :write-string-to-file))
 
 (in-package :cl-sbt)
 
-(defmacro container ((&key (fluid nil)) &body body)
-  "Bootstrap Grid Container. Set :fluid t for fluid layout."
-  `(spinneret:with-html
-     (:div :class ,(if fluid "container-fluid" "container")
-           ,@body)))
-
-(defmacro row (&body body)
-  "Use rows to create horizontal groups of columns."
-  `(spinneret:with-html
-     (:div :class "row"
-           ,@body)))
-
-(defmacro icon ((&key (glyph "asterisk")))
-  "Glyphicon from bootstrap"
-  `(spinneret:with-html
-     (:span :class (format nil "glyphicon glyphicon-~a" ,glyph))))
-
-(defmacro well (&body body)
-  "bootstrap well"
-  `(spinneret:with-html
-     (:div :class "well"
-           ,@body)))
+(defun write-string-to-file (filename string)
+  (with-open-file (stream filename :direction :output :if-exists :supersede)
+    (write-string string stream)))
