26658c5106e3c1485b0d0331037ed5d68e2878cb
Marcus Kammer <marcus.kammer@mailbox.org>
2023-07-20 08:32:44 +0200

    Add meta tags and use global parameters

--- src/main.lisp
+++ src/main.lisp
@@ -6,18 +6,23 @@
 
 (in-package :cl-sbt)
 
+(defparameter *cdn-css* "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css")
+(defparameter *cdn-js* "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js")
+
 (defmacro with-page ((&key cdn title) &body body)
   `(spinneret:with-html
      (:doctype)
      (:html
       (:head
-       (:title ,title ,cdn)
+       (:meta :charset "utf-8")
+       (:meta :name "viewport" :content "width=device-width, initial-scale=1")
+       (:title ,title)
        (if ,cdn
-           (:link :type "text/css" :rel "stylesheet" :href "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css")
+           (:link :type "text/css" :rel "stylesheet" :href ,*cdn-css*)
            (:link :type "text/css" :rel "stylesheet" :href "5.3.0/bootstrap.min.css")))
       (:body ,@body)
       (if ,cdn
-          (:script :src "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js")
+          (:script :src *cdn-js*)
           (:script :src "5.3.0/bootstrap.bundle.min.js")))))
 
 (defun write-string-to-file (filename string)
