b259ff5bc881c599192a54a8f2e0cdae2452466f
Marcus Kammer <marcus.kammer@mailbox.org>
2023-07-20 08:04:24 +0200

    Decide to use cdn to load bootstrap js and css

--- src/main.lisp
+++ src/main.lisp
@@ -6,15 +6,19 @@
 
 (in-package :cl-sbt)
 
-(defmacro with-page ((&key title) &body body)
+(defmacro with-page ((&key cdn title) &body body)
   `(spinneret:with-html
      (:doctype)
      (:html
       (:head
-       (:title ,title)
-       (:link :type "text/css" :rel "stylesheet" :href "public/5.3.0/bootstrap.min.css"))
+       (:title ,title ,cdn)
+       (if ,cdn
+           (:link :type "text/css" :rel "stylesheet" :href "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css")
+           (:link :type "text/css" :rel "stylesheet" :href "5.3.0/bootstrap.min.css")))
       (:body ,@body)
-      (:script :src "public/5.3.0/bootstrap.bundle.min.js"))))
+      (if ,cdn
+          (:script :src "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js")
+          (:script :src "5.3.0/bootstrap.bundle.min.js")))))
 
 (defun write-string-to-file (filename string)
   (with-open-file (stream filename :direction :output :if-exists :supersede)
