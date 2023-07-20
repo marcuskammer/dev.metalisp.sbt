5756553141cb2a27093ddb2f384779d7bc94ebd2
Marcus Kammer <marcus.kammer@mailbox.org>
2023-07-20 17:22:34 +0200

    Wrap hero in main

--- examples/album.lisp
+++ examples/album.lisp
@@ -182,9 +182,9 @@ to action button. The generated hero unit uses Bootstrap classes for styling."
                   (:url "#" :label "Like on Facebook")
                   (:url "#" :label "Email me"))))
 
-     (hero :title "Album example"
-           :lead "Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely."
-           :cta "Main call to action")
+     (:main (hero :title "Album example"
+                  :lead "Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely."
+                  :cta "Main call to action"))
 
      (footer ())))
 
