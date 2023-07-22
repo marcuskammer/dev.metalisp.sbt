45e817ca4ad903995d3d3eb57c777ca30164e538
Marcus Kammer <marcus.kammer@mailbox.org>
2023-07-22 13:58:32 +0200

    Remove toy test

--- tests/main.lisp
+++ tests/main.lisp
@@ -5,7 +5,3 @@
 (in-package :cl-sbt/tests/main)
 
 ;; NOTE: To run this test file, execute `(asdf:test-system :cl-sbt)' in your Lisp.
-
-(deftest test-target-1
-  (testing "should (= 1 1) to be true"
-    (ok (= 1 1))))
