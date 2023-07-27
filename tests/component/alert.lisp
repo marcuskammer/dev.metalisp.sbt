f3b8c3fd95d4758812a804c74ef28edce87b2fb3
Marcus Kammer <marcus.kammer@mailbox.org>
2023-07-27 14:31:41 +0200

    Test dismiss button

--- tests/component/alert.lisp
+++ tests/component/alert.lisp
@@ -19,3 +19,19 @@
       (ok (search "type=button" result))
       (ok (search "data-bs-dismiss=alert" result))
       (ok (search "aria-label=Close" result)))))
+
+(deftest test-btn-primary
+  (testing "Testing primary btn"
+    (let ((result (spinneret:with-html-string (alert-primary "foo"))))
+      (ok (search "class=\"alert alert-primary\"" result))
+      (ok (search "role=alert" result)))))
+
+(deftest test-btn-dismiss-primary
+  (testing "Testing dismiss primary btn"
+    (let ((result (spinneret:with-html-string (alert-dismiss-primary "foo"))))
+      (ok (search "class=\"alert alert-primary alert-dismissible\"" result))
+      (ok (search "role=alert" result))
+      (ok (search "class=btn-close" result))
+      (ok (search "type=button" result))
+      (ok (search "data-bs-dismiss=alert" result))
+      (ok (search "aria-label=Close" result)))))
