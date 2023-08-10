fc5e68dca566555bb5a8d95ba0312a368b1e1448
Marcus Kammer <marcus.kammer@mailbox.org>
2023-08-10 22:46:54 +0200

    Add form ctrls with columns

--- src/component/form.lisp
+++ src/component/form.lisp
@@ -2,7 +2,8 @@
   (:use
    :cl)
   (:export
-   :ctrl))
+   :ctrl
+   :ctrl-col))
 
 (in-package :cl-sbt/form)
 
@@ -10,6 +11,28 @@
   `(spinneret:with-html
      ,@(loop for item in rest
              collect (destructuring-bind (&key id label type placeholder) item
-                       `(progn
-                          (:label :for ,id :class "form-label" ,label)
-                          (:input :type ,type :class "form-control" :id ,id :placeholder ,placeholder))))))
+                       `(:div :class "mb-3"
+                              (:label :for ,id
+                                      :class "form-label"
+                                      ,label)
+                              (:input :type ,type
+                                      :class "form-control"
+                                      :id ,id
+                                      :placeholder ,placeholder))))))
+
+(defmacro ctrl-col (&rest rest)
+  `(spinneret:with-html
+     ,@(loop for item in rest
+             collect (destructuring-bind (&key id label type placeholder) item
+                       `(:div :class "row g-3 align-items-center"
+                              (:div :class "col-auto"
+                                    (:label :for ,id
+                                            :class "col-form-label"
+                                            ,label))
+                              (:div :class "col-auto"
+                                    (:label :type ,type
+                                            :id ,id
+                                            :class "form-control"))
+                              (:div :class "col-auto"
+                                    (:span :class "form-text"
+                                           ,text)))))))
