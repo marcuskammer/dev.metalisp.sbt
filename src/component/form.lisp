68db50a29ca8f5b1ccf2b519d8beffb8f4b2f8a5
Marcus Kammer <marcus.kammer@mailbox.org>
2023-08-11 09:59:56 +0200

    Add description text to forms

--- src/component/form.lisp
+++ src/component/form.lisp
@@ -10,15 +10,26 @@
 (defmacro ctrl (&rest rest)
   `(spinneret:with-html
      ,@(loop for item in rest
-             collect (destructuring-bind (&key id label type placeholder) item
+             collect (destructuring-bind (&key id label type placeholder text describeby) item
                        `(:div :class "mb-3"
                               (:label :for ,id
                                       :class "form-label"
                                       ,label)
-                              (:input :type ,type
-                                      :class "form-control"
-                                      :id ,id
-                                      :placeholder ,placeholder))))))
+                              ,(if describeby
+                                   `(:input :type ,type
+                                            :class "form-control"
+                                            :id ,id
+                                            :placeholder ,placeholder
+                                            :aria-describeby ,describeby)
+                                   `(:input :type ,type
+                                            :class "form-control"
+                                            :id ,id
+                                            :placeholder ,placeholder))
+                              ,(if text
+                                   `(:div :id ,describeby
+                                          :class "form-text"
+                                          ,text)
+                                   nil))))))
 
 (defmacro ctrl-col (&rest rest)
   `(spinneret:with-html
--- tests/component/form.lisp
+++ tests/component/form.lisp
@@ -19,16 +19,22 @@
       (ok (search "id=exampleFormControlInput1" result))
       (ok (search "placeholder=name@example.com" result)))))
 
+(deftest test-simple-form-control-with-text
+  (let ((result (spinneret:with-html-string (ctrl (:id "exampleFormControlInput1" :label "Email address" :type "email" :placeholder "name@example.com" :text "Your password must be 8-20 characters long" :describeby "passwordHelpBlock")))))
+    (testing "Generates correct HTML for a simple form with text"
+      (ok (search "for=exampleFormControlInput1" result))
+      (ok (search "class=form-label" result))
+      (ok (search "type=email" result))
+      (ok (search "class=form-control" result))
+      (ok (search "id=exampleFormControlInput1" result))
+      (ok (search "placeholder=name@example.com" result))
+      (ok (search "class=form-text" result))
+      (ok (search "Your password must" result))
+      (ok (search "aria-describeby=passwordHelpBlock" result))
+      (ok (search "id=passwordHelpBlock" result)))))
+
 (deftest test-simple-form-control-2
-  (let ((result (spinneret:with-html-string (ctrl
-                                             (:id "exampleFormControlInput1"
-                                              :label "Email address"
-                                              :type "email"
-                                              :placeholder "name@example.com")
-                                             (:id "form1"
-                                              :label "Another Form"
-                                              :type "email"
-                                              :placeholder "name@example.com")))))
+  (let ((result (spinneret:with-html-string (ctrl (:id "exampleFormControlInput1" :label "Email address" :type "email" :placeholder "name@example.com") (:id "form1" :label "Another Form" :type "email" :placeholder "name@example.com")))))
     (testing "Generates correct HTML for a simple form with more than one entries"
       (ok (search "for=exampleFormControlInput1" result))
       (ok (search "class=form-label" result))
