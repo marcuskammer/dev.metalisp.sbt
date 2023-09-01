1396c0d72f1eac491b5ca0a03bd73a36e1ff07fa
Marcus Kammer <marcus.kammer@mailbox.org>
2023-09-01 10:41:52 +0200

    Test item and accordion macro

--- tests/component/accordion.lisp
+++ tests/component/accordion.lisp
@@ -6,7 +6,9 @@
   (:import-from
    :cl-sbt/accordion
    :header
-   :collapse))
+   :collapse
+   :item
+   :accordion))
 
 (in-package :cl-sbt/tests/accordion)
 
@@ -27,3 +29,35 @@
       (ok (search "id=collapseOne" result))
       (ok (search "data-bs-parent=#accordionExample" result))
       (ok (search "class=accordion-body" result)))))
+
+(deftest test-item
+  (let ((result (spinneret:with-html-string (item (header "collapseOne" "Heading" t) (collapse "accordionExample" "collapseOne" t "Some content")))))
+    (testing "Generates correct HTML for accordion item"
+      (ok (search "class=accordion-item" result))
+      (ok (search "class=accordion-header" result))
+      (ok (search "class=accordion-button" result))
+      (ok (search "class=\"accordion-collapse collapse show\"" result))
+      (ok (search "data-bs-target=#collapseOne" result))
+      (ok (search "aria-expanded=true" result))
+      (ok (search "aria-controls=#collapseOne" result))
+      (ok (search "Some content" result)))))
+
+(deftest test-accordion
+  (let ((result (spinneret:with-html-string
+                  (accordion "accordionExample"
+                             (:target "collapseOne" :name "Accordion Item #1" :show t :content "This is the first item's accordion body.")
+                             (:target "collapseTwo" :name "Accordion Item #2" :content "This is the second item's accordion body.")
+                             (:target "collapseThree" :name "Accordion Item #3" :content "This is the third item's accordion body.")))))
+    (testing "Generates correct HTML for accordion"
+      (ok (search "class=accordion" result))
+      (ok (search "id=accordionExample" result))
+      (ok (search "class=accordion-item" result))
+      (ok (search "class=accordion-header" result))
+      (ok (search "class=accordion-button" result))
+      (ok (search "class=\"accordion-collapse collapse show\"" result))
+      (ok (search "data-bs-target=#collapseOne" result))
+      (ok (search "aria-expanded=true" result))
+      (ok (search "aria-controls=#collapseOne" result))
+      (ok (search "This is the first item&#39;s accordion body." result))
+      (ok (search "This is the second item&#39;s accordion body." result))
+      (ok (search "This is the third item&#39;s accordion body." result)))))
