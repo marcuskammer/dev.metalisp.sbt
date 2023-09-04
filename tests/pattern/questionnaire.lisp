fd47c29f8ac96cef03916d772c353a4fb25ad991
Marcus Kammer <marcus.kammer@mailbox.org>
2023-09-04 15:03:21 +0200

    Add test for questionnaire

--- tests/pattern/questionnaire.lisp
+++ tests/pattern/questionnaire.lisp
@@ -28,3 +28,20 @@
         (resolve-input-and-choices '("A" "B"))
       (ok (null type))
       (ok (equal choices '("A" "B"))))))
+
+(deftest test-create-questionnaire
+  (let ((result (spinneret:with-html-string
+                   (questionnaire "/submit"
+                                  (:ask "How old are you?"
+                                   :group "age"
+                                   :choices (:single "18-24" "25-34" "35-44" "45-54" "55+"))
+                                  (:ask "Your Gender?"
+                                   :group "gender"
+                                   :choices (:single "Male" "Female" "Non-Binary" "Prefer not to say"))))))
+    (testing "Generates correct HTML for questionnaire"
+      (ok (search "<form class=py-5 action=/submit method=post>" result))
+      (ok (search "<legend>How old are you?</legend>" result))
+      (ok (search "<input type=radio name=group-age value=18_24> 18-24</label>" result))
+      (ok (search "<input type=radio name=group-gender value=Male> Male</label>" result))
+      (ok (search "<hr class=my-4>" result))
+      (ok (search "<button class=\"btn btn-primary\" type=submit>Submit</button>" result)))))
