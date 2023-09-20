b5c9fdf2948548d3462d368f93e6a3499608735b
Marcus Kammer <marcus.kammer@mailbox.org>
2023-09-20 13:31:16 +0200

    Fix return type

--- src/pattern/questionnaire.lisp
+++ src/pattern/questionnaire.lisp
@@ -228,7 +228,7 @@ Returns:
     ;; Return the reversed result list
     (nreverse result)))
 
-(declaim (ftype (function (question) (values string string string))
+(declaim (ftype (function (question) (values string string list))
                 extract-question-components))
 (defun extract-question-components (question)
   "Extracts components of a question stored as a plist.
