97b0fc8399cda3e52bae2cce7636519a762ecf79
Marcus Kammer <marcus.kammer@mailbox.org>
2023-11-09 11:04:03 +0100

    Describe all type specifiers for input

--- src/component/form.lisp
+++ src/component/form.lisp
@@ -152,7 +152,9 @@ checkable macros."
 (defun ctrl (type name label)
   "Generates a basic Bootstrap form control with a label.
 
-TYPE: Specifies the type of input, such as 'text', 'password', etc.
+TYPE: Specifies the type of input, such as: 'button', 'checkbox', 'color', 'date', 'datetime-local',
+'email', 'file', 'hidden', 'image', 'month', 'number', 'password', 'radio', 'range', 'reset',
+'search', 'submit', 'tel', 'text', 'time', 'url', 'week'.
 
 NAME: The name attribute for the control.
 
