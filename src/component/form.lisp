2e1d9b4ec2588ab2a7e40922fd52854747b64936
Marcus Kammer <marcus.kammer@mailbox.org>
2023-11-09 11:12:28 +0100

    Fix indentation

--- src/component/form.lisp
+++ src/component/form.lisp
@@ -154,7 +154,7 @@ checkable macros."
 
 TYPE: Specifies the type of input, such as: 'button', 'checkbox', 'color', 'date', 'datetime-local',
 'email', 'file', 'hidden', 'image', 'month', 'number', 'password', 'radio', 'range', 'reset',
-'search', 'submit', 'tel', 'text', 'time', 'url', 'week'.
+'search', 'submit', 'tel' 'text', 'time', 'url', 'week'
 
 NAME: The name attribute for the control.
 
