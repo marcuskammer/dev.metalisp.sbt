b75cd0748540876cedc39034d3a12869efab924b
Marcus Kammer <marcus.kammer@mailbox.org>
2023-12-18 11:44:44 +0100

    Rename package navbar

--- src/component/navbar.lisp
+++ src/component/navbar.lisp
@@ -25,7 +25,7 @@
 ;; Form Support: You can include form elements directly in your Navbar, allowing
 ;; users to input data directly from the navigation bar.
 
-(defpackage dev.metalisp.sbt/navbar
+(defpackage dev.metalisp.sbt/component/navbar
   (:use :cl)
   (:import-from
    :dev.metalisp.sbt/component/btn
@@ -33,7 +33,7 @@
   (:import-from
    :dev.metalisp.sbt/layout/grid
    :con
-   :row
+   :row)
   (:export
    :navbar
    :brand
@@ -45,7 +45,7 @@
    :form-search
    :collapsible))
 
-(in-package :dev.metalisp.sbt/navbar)
+(in-package :dev.metalisp.sbt/component/navbar)
 
 (defmacro brand-logo ((&key (src "#") (alt "Logo") (width 30) (height 24) (classes "d-inline-block align-text-top")))
   "This macro generates a brand logo for a Bootstrap navbar.
