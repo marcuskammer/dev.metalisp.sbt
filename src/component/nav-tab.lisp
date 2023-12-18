93cd323d9163f30c2d87d5d888b59dae79a83917
Marcus Kammer <marcus.kammer@mailbox.org>
2023-12-18 12:14:10 +0100

    Rename package nav-tabs

--- src/component/nav-tab.lisp
+++ src/component/nav-tab.lisp
@@ -33,13 +33,13 @@
 ;; adjusts its layout based on the size of the user's screen. This makes it a
 ;; flexible choice for a variety of web layouts.
 
-(defpackage dev.metalisp.sbt/nav
+(defpackage dev.metalisp.sbt/component/nav
   (:use :cl)
   (:export
    :item
    :nav))
 
-(in-package :dev.metalisp.sbt/nav)
+(in-package :dev.metalisp.sbt/component/nav)
 
 (defmacro item (name active url)
   "This macro generates a navigation item for a Bootstrap navigation list.
