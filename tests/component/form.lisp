5bd5489794649c0fe94ab5c631880c0b94184cf0
Marcus Kammer <marcus.kammer@mailbox.org>
2023-12-22 14:11:08 +0100

    Add more tests for component/form

--- tests/component/form.lisp
+++ tests/component/form.lisp
@@ -6,6 +6,10 @@
   (:import-from
    :dev.metalisp.sbt/component/form
    :remove-special-chars
+   :clean-form-str
+   :build-name-str
+   :build-value-str
+   :build-value-prop-str
    :ctrl
    :combo
    :combo-sm
@@ -18,6 +22,22 @@
 (deftest test-remove-special-chars
   (testing "Removes all special characters from the string STR except numbers and alphabets."
     (ok (string= (remove-special-chars "a1b!@#$%^&*()c2") "a1bc2"))))
+
+(deftest test-clean-form-str
+  (testing "Cleans a form string for use as a name or identifier."
+    (ok (string= (clean-form-str " hello WORLD") "hello-world"))))
+
+(deftest test-build-name-str
+  (testing "Builds a standardized string by adding a 'group-' prefix and applying cleaning functions."
+    (ok (string= (build-name-str "somename") "group-somename"))))
+
+(deftest test-build-value-str
+  (testing "Trims leading and trailing spaces from the given value string."
+    (ok (string= (build-value-str " hello-world ") "hello-world"))))
+
+(deftest test-build-value-prop-str
+  (testing "Builds a value property string by applying various cleaning functions."
+    (ok (string= (build-value-prop-str " hello world ") "hello-world"))))
 
 (deftest test-select-default
   (let ((result (spinneret:with-html-string (combo () "Red" "Green" "Blue"))))
