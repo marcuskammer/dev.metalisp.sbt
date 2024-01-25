251fd4e7fd342213451fc1796fc8f4578c3ae13b
Marcus Kammer <marcus.kammer@evosoft.com>
2024-01-25 16:07:26 +0100

    Clean up page package

--- src/main.lisp
+++ src/main.lisp
@@ -4,18 +4,48 @@
 (defpackage dev.metalisp.sbt
   (:use :cl)
   (:export
-   :*cdn-css*
-   :*cdn-js*
+   :*use-cdn*
+   :*cdn-css-url*
+   :*cdn-js-url*
+   :*bs-version*
+   :*color-theme*
    :write-html-to-file
-   :with-page
-   :*l10n*
-   :find-l10n))
 
 (in-package :dev.metalisp.sbt)
 
 (setq spinneret:*fill-column* 120)
 
-(defun write-html-to-file (filename string &key (lang "en") (style :tree) (fc 120))
+(defparameter *bs-version* "5.3.2")
+
+(defparameter *use-cdn* t)
+
+(defparameter *cdn-css-url*
+  (concatenate 'string
+               "https://cdn.jsdelivr.net/npm/bootstrap@"
+               *bs-version*
+               "/dist/css/bootstrap.min.css"))
+
+(defparameter *cdn-js-url*
+  (concatenate 'string
+               "https://cdn.jsdelivr.net/npm/bootstrap@"
+               *bs-version*
+               "/dist/js/bootstrap.bundle.min.js"))
+
+(defparameter *color-theme* "dark")
+
+(defun download-file (url directory)
+  "Downloads a file from a given URL and saves it to the specified directory."
+  (let* ((filename (car (last (uiop:split-string url :separator "/"))))
+         (filepath (merge-pathnames filename directory)))
+    (uiop:ensure-all-directories-exist directory)
+    (with-open-file (stream filepath
+                            :direction :output
+                            :if-exists :supersede
+                            :element-type '(unsigned-byte 8))
+      (dex:get url :stream stream))
+    filepath))
+
+(defun write-html-str-to-file (filename string &key (lang "en") (style :tree) (fc 120))
   (let ((spinneret:*html-lang* lang)
         (spinneret:*html-style* style)
         (spinneret:*fill-column* fc))
--- src/page.lisp
+++ src/page.lisp
@@ -3,67 +3,36 @@
 
 (defpackage dev.metalisp.sbt/page
   (:use :cl)
+  (:import-from
+   :dev.metalisp.sbt
+   :*color-themes*
+   :*use-cdn*
+   :*cdn-css-url*
+   :*cdn-js-url*)
   (:export
-   :with-page
-   :with-landing-page
-   :with-blogpost-page
-   :with-product-page
-   :with-contact-page
-   :with-portfolio-page
-   :with-error-page
-   :with-login-page
-   :with-dashboard-page
-   :with-about-page
-   :with-faq-page
-   :with-tos-page))
+   :with-page))
 
 (in-package :dev.metalisp.sbt/page)
 
-(defparameter *bs-version* "5.3.2")
-
-(defparameter *use-cdn* t)
-
-(defparameter *cdn-css-url*
-  (concatenate 'string
-               "https://cdn.jsdelivr.net/npm/bootstrap@"
-               *bs-version*
-               "/dist/css/bootstrap.min.css"))
-
-(defparameter *cdn-js-url*
-  (concatenate 'string
-               "https://cdn.jsdelivr.net/npm/bootstrap@"
-               *bs-version*
-               "/dist/js/bootstrap.bundle.min.js"))
-
-(defparameter *color-theme* "dark")
-
-(defmacro with-page ((&key (author "") (description "") (pagetitle "")) &body body)
+(defmacro with-page ((&key pagemeta pagetitle pagecss pagejs) &body body)
   `(spinneret:with-html
      (:doctype)
      (:html :data-bs-theme ,*color-theme*
             (:head
              (:meta :charset "utf-8")
              (:meta :name "viewport" :content "width=device-width, initial-scale=1")
-             (:meta :name "author" :content ,author)
-             (:meta :name "description" :content ,description)
+             ,@(loop for (key value) on pagemeta by #'cddr
+                     collect `(:meta :name ,(string-downcase (symbol-name key)) :content ,(getf pagemeta key)))
              (:title ,pagetitle)
              (if *use-cdn*
                  (:link :type "text/css" :rel "stylesheet" :href ,*cdn-css-url*)
                  (:link :type "text/css" :rel "stylesheet" :href "5.3.0/bootstrap.min.css"))
+             ,@(loop for css in pagecss
+                     collect `(:link :type "text/css" :rel "stylesheet" :href ,css))
              (:body (:h1 :class "visually-hidden" ,pagetitle)
                (:main ,@body))
              (if ,*use-cdn*
                  (:script :src *cdn-js-url*)
-                 (:script :src "5.3.0/bootstrap.bundle.min.js"))))))
-
-(defmacro with-landing-page ())
-(defmacro with-blogpost-page ())
-(defmacro with-product-page ())
-(defmacro with-contact-page ())
-(defmacro with-portfolio-page ())
-(defmacro with-error-page ())
-(defmacro with-login-page ())
-(defmacro with-dashboard-page ())
-(defmacro with-about-page ())
-(defmacro with-faq-page ())
-(defmacro with-tos-page ())
+                 (:script :src "5.3.0/bootstrap.bundle.min.js"))
+             ,@(loop for js in pagejs
+                     collect `(:script :src ,js))))))
