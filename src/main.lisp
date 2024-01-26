1fb5193d46f7d2ab997ed90bc6324149caaa5485
Marcus Kammer <marcus.kammer@evosoft.com>
2024-01-26 16:30:01 +0100

    Update with-page macro

--- src/main.lisp
+++ src/main.lisp
@@ -9,7 +9,8 @@
    :*cdn-js-url*
    :*bs-version*
    :*color-theme*
-   :write-html-to-file))
+   :write-html-to-file
+   :with-page))
 
 (in-package :dev.metalisp.sbt)
 
@@ -31,7 +32,23 @@
                *bs-version*
                "/dist/js/bootstrap.bundle.min.js"))
 
+(defparameter *local-css-url*
+  "5.3.0/bootstrap.min.css")
+
+(defparameter *local-js-url*
+  "5.3.0/bootstrap.bundle.min.js")
+
 (defparameter *color-theme* "dark")
+
+(defun css-url ()
+  (if *use-cdn*
+      *cdn-css-url*
+      *local-css-url*))
+
+(defun js-url ()
+  (if *use-cdn*
+      *cdn-js-url*
+      *local-js-url*))
 
 (defun download-file (url directory)
   "Downloads a file from a given URL and saves it to the specified directory."
@@ -51,3 +68,25 @@
         (spinneret:*fill-column* fc))
     (with-open-file (stream filename :direction :output :if-exists :supersede)
       (write-string string stream))))
+
+(defmacro with-page ((&key meta title add-css-urls add-js-urls) &body body)
+  `(spinneret:with-html
+     (:doctype)
+     (:html :data-bs-theme ,*color-theme*
+            (:head (:meta :charset "utf-8")
+                   (:meta :name "viewport" :content "width=device-width, initial-scale=1")
+                   ,@(loop for (key value) on meta by #'cddr
+                           collect `(:meta :name ,(string-downcase (symbol-name key)) :content ,(getf meta key)))
+
+                   (:title ,title)
+
+                   (:link :type "text/css" :rel "stylesheet" :href ,(css-url))
+                   ,@(loop for url in add-css-urls
+                           collect `(:link :type "text/css" :rel "stylesheet" :href ,url)))
+
+            (:body (:h1 :class "visually-hidden" ,title)
+              (:main ,@body)
+
+              (:script :src ,(js-url))
+              ,@(loop for url in add-js-urls
+                      collect `(:script :src ,url))))))
