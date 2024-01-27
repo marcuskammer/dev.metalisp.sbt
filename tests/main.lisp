b1709898129babe09b41b4ca142bfda6ff86f605
Marcus Kammer <marcus.kammer@mailbox.org>
2024-01-27 21:30:25 +0100

    Add test for with-page

--- tests/main.lisp
+++ tests/main.lisp
@@ -5,3 +5,11 @@
 (in-package :dev.metalisp.sbt/tests/main)
 
 ;; NOTE: To run this test file, execute `(asdf:test-system :dev.metalisp.sbt)' in your Lisp.
+
+(deftest test-with-page
+  (let ((result (with-output-to-string (spinneret:*html*)
+                  (with-page (:title "Example")
+                    (:h2 "Hello World")))))
+    (testing "Testing with-page macro"
+      (ok (search "<title>Example</title>" result))
+      (ok (search "<h2>Hello World</h2>" result)))))
