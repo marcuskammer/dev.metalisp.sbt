5844193ed996757bb8c6af376d9eb87fe478ce09
Marcus Kammer <marcus.kammer@mailbox.org>
2024-04-04 17:07:10 +0200

    Update mode line

--- src/bi.lisp
+++ src/bi.lisp
@@ -1,4 +1,4 @@
-;;;; -*- mode: lisp; coding: utf-8; tab-width: 4; fill-column: 100; indent-tabs-mode: nil; -*-
+;;;; -*- mode: lisp; coding: utf-8; fill-column: 84; indent-tabs-mode: nil; -*-
 ;;;; bi.lisp
 ;;;; Provide functions and macros related to bootstrap icons.
 
