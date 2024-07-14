44c49f0a932ac8e01e37234f46e8a38ce97fba6f
Marcus Kammer <marcus.kammer@mailbox.org>
2024-07-14 15:44:24 +0200

    Detect dark or light theme

--- src/main.lisp
+++ src/main.lisp
@@ -158,7 +158,10 @@ Example usage:
                    (:link :type "text/css" :rel "stylesheet" :href ,(bs-url-css))
                    ,@(loop for url in add-css-urls
                            collect `(:link :type "text/css"
-                                      :rel "stylesheet" :href ,url)))
+                                      :rel "stylesheet" :href ,url))
+
+                   (:script (:raw "(()=>{'use strict';const getStoredTheme=()=>localStorage.getItem('theme');const getPreferredTheme=()=>{const storedTheme=getStoredTheme();if(storedTheme){return storedTheme}return window.matchMedia('(prefers-color-scheme: dark)').matches?'dark':'light'};const setTheme=theme=>{if(theme==='auto'){document.documentElement.setAttribute('data-bs-theme',(window.matchMedia('(prefers-color-scheme: dark)').matches?'dark':'light'))}else{document.documentElement.setAttribute('data-bs-theme',theme)}};setTheme(getPreferredTheme())})();")))
+
             (:body :class "py-3" ,@body)
 
             (:script :src ,(bs-url-js))
