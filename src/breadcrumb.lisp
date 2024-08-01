e221d0dae567554933a706b0257626c71f3fcc4f
Marcus Kammer <marcus.kammer@mailbox.org>
2024-08-01 20:54:52 +0200

    Fix docstring

--- src/breadcrumb.lisp
+++ src/breadcrumb.lisp
@@ -7,13 +7,13 @@
 (defmacro with-breadcrumb (&rest items)
   "Creates a Bootstrap breadcrumb navigation.
 
-ITEMS: A list of (label . url) pairs. The last item is automatically set as active.
+ITEMS: A list of \"label\" \"url\" pairs. The last item is automatically set as active.
 
 Example usage:
   (with-breadcrumb
-    (\"Home\" . \"/\")
-    (\"Library\" . \"/library\")
-    (\"Data\" . nil))"
+    \"Home\" \"/\"
+    \"Library\" \"/library\"
+    \"Data\" nil)"
   `(spinneret:with-html
      (:nav :class "container"
            :aria-label "breadcrumb"
